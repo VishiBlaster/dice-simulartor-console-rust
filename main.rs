1575d02444684e9be61b2301055445cd4bbae40b
VishiBlaster <mongajatin2588@gmail.com>
2022-10-29 10:05:27 +0530

    added code but have to do debugging

--- main.rs
+++ main.rs
@@ -1,10 +1,13 @@
+use std::io;
+use rand::Rng;
 fn main() {
     let mut comp = 0 ;
-    let mut hum = 0;
-    if comp = hum {
-        println!("same number. YOu are ass smart as the computer");
+    let mut hum : i32;
+    io::stdin().read_line(&mut hum).expect("You entered the wrong value :(");
+    if comp == hum {
+        println!("same number. You are ass smart as the computer");
     } else {
         println!("try again not as smart as the computer") ;
     }
     }
-    
+    //io::stdin().read_line(&mut guess).expect("failed to readline");
