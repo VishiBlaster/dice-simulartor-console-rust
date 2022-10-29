dd194d6d630712da5ea80069a1124c484b440452
VishiBlaster <mongajatin2588@gmail.com>
2022-10-29 09:27:31 +0530

    added code but not compiled and tested

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut comp = 0 ;
+    let mut hum = 0;
+    if comp = hum {
+        println!("same number. YOu are ass smart as the computer");
+    } else {
+        println!("try again not as smart as the computer") ;
+    }
+    }
+    
